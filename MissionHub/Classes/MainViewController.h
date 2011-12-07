ec695eba9c2d5aa155f11152712dba22fad6edbc
David Ang <davidang09@gmail.com>
2011-12-07 09:35:06 +0800

    added mainview controller added initial iephone nib version for main view

---
+++ MissionHub/Classes/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  MissionHub
+//
+//  Created by David Ang on 12/7/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
